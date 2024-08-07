e51b502d180a6e112de9001e26b0649652da675a
Hoora Akhlaghi <hakhlaghi83@gmail.com>
2024-08-07 23:24:03 +0330

    add answer of task3

---
+++ exercises/3/answers/Hoora/1.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+if [ -f /etc/os-release ]
+then
+	. /etc/os-release
+	DISTRO=$ID
+else
+	exit 0
+fi
+
+if [ $(-z "which dilog") ] | [ $(-z "which bc")]
+then
+	echo "salam"
+	if [ $(DISTRO)=="arch" ] | [ $(DISTRO)=="manjaro"]
+	then
+		sudo pacman -Syyu
+		sudo pacman -S dialog bc
+	elif [ $(DISTRO)=="ubuntu" ] | [ $(DISTRO)=="debian" ] | [ $(DISTRO)=="linuxmint" ] | [ $(DISTRO)=="raspbian" ]
+	then
+		sudo apt update
+		sudo apt install -y dialog bc
+	elif [ $(DISTRO)=="fedora" ] | [ $(DISTRO)=="rhel" ] | [ $(DISTRO)=="centos" ]
+	then
+		sudo dnf install -y dialog bc
+	elif [ $(DISTRO)=="opensuse" ]
+	then
+		sudo zypper install -y dialog bc
+	else
+		exit 0
+	fi
+fi
+
+
+touch tempfile.txt
+
+dialog --inputbox "number1:" 8 40 0> tempfile.txt
+NUMBER1=$(<tempfile.txt)
+dialog --inputbox "operator:" 8 40 0> tempfile.txt
+OPERATOR=$(<tempfile.txt)
+dialog --inputbox "number2:" 8 40 0> tempfile.txt
+NUMBER2=$(<tempfile.txt)
+
+rm tempfile.txt
+
+dialog --msgbox "result : $(echo "${NUMBER1} ${OPERATOR} ${NUMBER2}" | bc)" 8 40
+clear
