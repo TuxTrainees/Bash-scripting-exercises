27a5509508db02d0e71dd443686cd777aadc6c3b
Hoora Akhlaghi <hakhlaghi83@gmail.com>
2024-08-08 17:13:24 +0330

    modify answer of task3

--- exercises/3/answers/Hoora/1.sh
+++ exercises/3/answers/Hoora/1.sh
@@ -5,39 +5,37 @@ then
 	. /etc/os-release
 	DISTRO=$ID
 else
-	exit 0
+	exit 2
 fi
 
 if [ $(-z "which dilog") ] | [ $(-z "which bc")]
 then
-	echo "salam"
-	if [ $(DISTRO)=="arch" ] | [ $(DISTRO)=="manjaro"]
+	if [ "$DISTRO" = "arch" ] | [ "$DISTRO" ="manjaro"]
 	then
 		sudo pacman -Syyu
 		sudo pacman -S dialog bc
-	elif [ $(DISTRO)=="ubuntu" ] | [ $(DISTRO)=="debian" ] | [ $(DISTRO)=="linuxmint" ] | [ $(DISTRO)=="raspbian" ]
+	elif [ "$DISTRO" = "ubuntu" ] | [ "$DISTRO" = "debian" ] | [ "$DISTRO" = "linuxmint" ] | [ "$DISTRO" = "raspbian" ]
 	then
 		sudo apt update
 		sudo apt install -y dialog bc
-	elif [ $(DISTRO)=="fedora" ] | [ $(DISTRO)=="rhel" ] | [ $(DISTRO)=="centos" ]
+	elif [ "$DISTRO" = "fedora" ] | [ "$DISTRO" = "rhel" ] | [ "$DISTRO" = "centos" ]
 	then
 		sudo dnf install -y dialog bc
-	elif [ $(DISTRO)=="opensuse" ]
+	elif [ "$DISTRO" = "opensuse" ]
 	then
 		sudo zypper install -y dialog bc
 	else
-		exit 0
+		echo "your distro is not found"
+		exit 2
 	fi
 fi
 
-
 touch tempfile.txt
-
-dialog --inputbox "number1:" 8 40 0> tempfile.txt
+dialog --inputbox "number1:" 8 40 2> tempfile.txt
 NUMBER1=$(<tempfile.txt)
-dialog --inputbox "operator:" 8 40 0> tempfile.txt
+dialog --inputbox "operator:" 8 40 2> tempfile.txt
 OPERATOR=$(<tempfile.txt)
-dialog --inputbox "number2:" 8 40 0> tempfile.txt
+dialog --inputbox "number2:" 8 40 2> tempfile.txt
 NUMBER2=$(<tempfile.txt)
 
 rm tempfile.txt
