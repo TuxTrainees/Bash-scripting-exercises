8c4cadf777e77cd6410ae492cdbacea22045b857
Hoora Akhlaghi <hakhlaghi83@gmail.com>
2024-08-09 13:43:33 +0330

    modify answer of task2

--- exercises/2/answers/Hoora/1.sh
+++ exercises/2/answers/Hoora/1.sh
@@ -1,16 +1,16 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-PATH=$1
+INPUT_PATH=$1
 PATH_BACKUP="/home/hoora/Desktop/backup"
 
-if [ ! -d "$PATH" ]
+if test ! -d "$INPUT_PATH"
 then
 	echo "${PATH} is not exist"
 	exit 2	
 fi
 
-GZ_FILENAME="$(basename ${PATH})-$(date +"%Y%m%d").tar.gz"
-tar -czf ${GZ_FILENAME} ${PATH}
+GZ_FILENAME="$(basename ${INPUT_PATH})-$(date +"%Y%m%d").tar.gz"
+tar -czf ${GZ_FILENAME} ${INPUT_PATH}
 
 mkdir -p PATH_BACKUP 2> /dev/null
 mv ${GZ_FILENAME} PATH_BACKUP
--- exercises/3/answers/Hoora/1.sh
+++ exercises/3/answers/Hoora/1.sh
@@ -1,24 +1,19 @@
 #!/usr/bin/env bash
 
-if [ -f /etc/os-release ]
-then
-	. /etc/os-release
-	DISTRO=$ID
-else
-	exit 2
-fi
+. /etc/os-release
+DISTRO=$ID
 
-if [ $(-z "which dilog") ] | [ $(-z "which bc")]
+if [ $(-z "which dilog") ] || [ $(-z "which bc")]
 then
-	if [ "$DISTRO" = "arch" ] | [ "$DISTRO" ="manjaro"]
+	if [ "$DISTRO" = "arch" ] || [ "$DISTRO" ="manjaro" ]
 	then
 		sudo pacman -Syyu
-		sudo pacman -S dialog bc
-	elif [ "$DISTRO" = "ubuntu" ] | [ "$DISTRO" = "debian" ] | [ "$DISTRO" = "linuxmint" ] | [ "$DISTRO" = "raspbian" ]
+		sudo pacman -Sy dialog bc
+	elif [ "$DISTRO" = "ubuntu" ] || [ "$DISTRO" = "debian" ] || [ "$DISTRO" = "linuxmint" ] || [ "$DISTRO" = "raspbian" ]
 	then
 		sudo apt update
 		sudo apt install -y dialog bc
-	elif [ "$DISTRO" = "fedora" ] | [ "$DISTRO" = "rhel" ] | [ "$DISTRO" = "centos" ]
+	elif [ "$DISTRO" = "fedora" ] || [ "$DISTRO" = "rhel" ] || [ "$DISTRO" = "centos" ]
 	then
 		sudo dnf install -y dialog bc
 	elif [ "$DISTRO" = "opensuse" ]
@@ -30,15 +25,15 @@ then
 	fi
 fi
 
-touch tempfile.txt
-dialog --inputbox "number1:" 8 40 2> tempfile.txt
-NUMBER1=$(<tempfile.txt)
-dialog --inputbox "operator:" 8 40 2> tempfile.txt
-OPERATOR=$(<tempfile.txt)
-dialog --inputbox "number2:" 8 40 2> tempfile.txt
-NUMBER2=$(<tempfile.txt)
+TMP_FILE="/tmp/tempfile.txt"
+touch $TMP_FILE
 
-rm tempfile.txt
+dialog --inputbox "number1:" 8 40 2> $TMP_FILE
+NUMBER1=$(<$TMP_FILE)
+dialog --inputbox "operator:" 8 40 2> $TMP_FILE
+OPERATOR=$(<$TMP_FILE)
+dialog --inputbox "number2:" 8 40 2> $TMP_FILE
+NUMBER2=$(<$TMP_FILE)
 
 dialog --msgbox "result : $(echo "${NUMBER1} ${OPERATOR} ${NUMBER2}" | bc)" 8 40
 clear
