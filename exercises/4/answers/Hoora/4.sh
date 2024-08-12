37572d08c111def1d52acbc764f4eb1355bbfa8b
Hoora Akhlaghi <hakhlaghi83@gmail.com>
2024-08-12 12:23:31 +0330

    modify answer of task4

--- exercises/4/answers/Hoora/4.sh
+++ exercises/4/answers/Hoora/4.sh
@@ -2,40 +2,29 @@
 
 echo -e "\033[1mLIST OF USERS:\033[0m"
 
-for USR in $(cut -d: -f1 /etc/passwd)
+USERS=$(cut -d ':' -f 1 /etc/passwd)
+
+for USER in ${USERS[@]}
 do
-	TOTAL_FILES=$(find /home -type f -user $USR | wc -l)
-	USER_HOME="/home/${USR}"
+	TOTAL_FILES=$(find /home -type f -user $USER | wc -l)
+	USER_HOME="/home/${USER}"
 
 	if [ -d ${USER_HOME} ]
 	then
-		TOTAL_DISK_USAGE=$(du -sh ${USER_HOME} | cut -f1)
+		TOTAL_DISK_USAGE=$(du -sh ${USER_HOME} | cut -f 1)
 	else
 		TOTAL_DISK_USAGE=0
 	fi
-	
-#	TOTAL_DISK_USAGE=0
-#	for FILE in $(find /home -type f -user $USR)
-#	do
-#		((TOTAL_DISK_USAGE += $(sudo du -s ${FILE} | cut -f1)))  		
-#	done	
 
-#	if [ -z $(lastlog --before 7 | grep ${USR}) ]
-#	then
-#		ACCOUNT_STATUS="inactive"
-#	else
-#		ACCOUNT_STATUS="active"
-#	fi
-
-	if [ -z $(last -s "1 week ago" | grep ${USR}) ]
+	if [[ $(last -s "1 week ago" | grep ${USER}) ]]
 	then
-		ACCOUNT_STATUS="inactive"
-	else
 		ACCOUNT_STATUS="active"
+	else
+		ACCOUNT_STATUS="inactive"
 	fi
 
 
-	echo -e "\n\tuser : ${USR}"
+	echo -e "\n\tuser : ${USER}"
 	echo -e "\ttotal files : ${TOTAL_FILES}"
 	echo -e "\ttotal disk usage : ${TOTAL_DISK_USAGE}"
 	echo -e "\taccount status : ${ACCOUNT_STATUS}"
